ec66c678496c775b56cc90383740e285c0ff5060
ttr <tuan-si.tran@senacor.com>
2017-05-30 14:14:36 +0200

    Added automatic Dockerfile creation in build_all_docker_containers.sh

--- scripts/build_all_docker_containers.sh
+++ scripts/build_all_docker_containers.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 dockerTagPrefix=$1
 
+port=8081
+
 #Building docker containers microservices
-microservices=("accounting" "portfolio" "office" "customer" "identity" "provisioner")
+microservices=("identity" "office" "customer" "accounting" "portfolio" "provisioner")
 for ms in "${microservices[@]}"
 do
-    echo Building docker container $ms
+    if [ "$ms" == "provisioner" ] ; then
+        port=9090
+    fi
     cd $ms
+    echo ::::: Create Dockerfile $ms
+    echo 'FROM openjdk:8-jre-alpine' > Dockerfile
+    echo 'VOLUME /tmp' >> Dockerfile
+    echo 'ADD service/build/libs/service-0.1.0-BUILD-SNAPSHOT-boot.jar app.jar' >> Dockerfile
+    echo "RUN /bin/sh -c 'touch /app.jar\'" >> Dockerfile
+    echo 'ENV JAVA_OPTS=""' >> Dockerfile
+    echo 'EXPOSE '${port} >> Dockerfile
+    echo 'ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]' >> Dockerfile
+    echo ::::: Building docker container $ms
     docker build -t $dockerTagPrefix/$ms . 
     cd ..
+
+    port=$(($port + 1))
+    echo $port
 done
